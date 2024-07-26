dd607d3cba4ebb11409d4f7b8bfcfed19d877d3c
hszadafiya <hszadafiya@mysenec.ca>
2024-07-26 15:56:22 -0400

    commit

---
+++ server.js
@@ -0,0 +1,142 @@
+/*********************************************************************************
+*  WEB700 – Assignment 05
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+* Name: Heet Zadafiya
+* Student ID: 140255233  Date: 26 Jun 2024
+*
+* Online (vercel) Link: 
+*
+********************************************************************************/
+
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const data = require("./modules/collegeData.js");
+
+const app = express();
+const HTTP_PORT = process.env.PORT || 8080;
+
+// Configure Handlebars
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    helpers: {
+        navLink: function(url, options) {
+            return '<li' +
+                ((url == app.locals.activeRoute) ? ' class="nav-item active"' : ' class="nav-item"') +
+                '><a class="nav-link" href="' + url + '">' + options.fn(this) + '</a></li>';
+        },
+        equal: function(lvalue, rvalue, options) {
+            if (arguments.length < 3)
+                throw new Error("Handlebars Helper equal needs 2 parameters");
+            if (lvalue != rvalue) {
+                return options.inverse(this);
+            } else {
+                return options.fn(this);
+            }
+        }
+    }
+}));
+
+app.set('view engine', '.hbs');
+
+// Middleware to serve static files and parse form data
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to set the active route
+app.use((req, res, next) => {
+    let route = req.path.substring(1);
+    app.locals.activeRoute = "/" + (isNaN(route.split('/')[1]) ? route.replace(/\/(?!.*)/, "") : route.replace(/\/(.*)/, ""));
+    next();
+});
+
+// Routes
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/htmlDemo", (req, res) => {
+    res.render("htmlDemo");
+});
+
+app.get("/students", (req, res) => {
+    if (req.query.course) {
+        data.getStudentsByCourse(req.query.course).then((data) => {
+            res.render("students", { students: data });
+        }).catch((err) => {
+            res.render("students", { message: "no results" });
+        });
+    } else {
+        data.getAllStudents().then((data) => {
+            res.render("students", { students: data });
+        }).catch((err) => {
+            res.render("students", { message: "no results" });
+        });
+    }
+});
+
+app.get("/students/add", (req, res) => {
+    res.render("addStudent");
+});
+
+app.post("/students/add", (req, res) => {
+    data.addStudent(req.body).then(() => {
+        res.redirect("/students");
+    }).catch((err) => {
+        res.status(500).send("Unable to add student");
+    });
+});
+
+app.get("/student/:studentNum", (req, res) => {
+    data.getStudentByNum(req.params.studentNum).then((data) => {
+        res.render("student", { student: data });
+    }).catch((err) => {
+        res.render("student", { message: "no results" });
+    });
+});
+
+app.post("/student/update", (req, res) => {
+    data.updateStudent(req.body).then(() => {
+        res.redirect("/students");
+    }).catch((err) => {
+        res.status(500).send("Unable to update student");
+    });
+});
+
+app.get("/courses", (req, res) => {
+    data.getCourses().then((data) => {
+        res.render("courses", { courses: data });
+    }).catch((err) => {
+        res.render("courses", { message: "no results" });
+    });
+});
+
+app.get("/course/:id", (req, res) => {
+    data.getCourseById(req.params.id).then((data) => {
+        res.render("course", { course: data });
+    }).catch((err) => {
+        res.render("course", { message: "no results" });
+    });
+});
+
+// 404 Handler
+app.use((req, res) => {
+    res.status(404).send("Page Not Found");
+});
+
+// Initialize data and start server
+data.initialize().then(() => {
+    app.listen(HTTP_PORT, () => {
+        console.log("app listening on: " + HTTP_PORT);
+    });
+}).catch((err) => {
+    console.log("unable to start server: " + err);
+});
